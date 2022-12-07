1a09c7a749b8134ab846c503596ddecb63eb7b4f
Thomas Dejardins <tdejardins@gmail.com>
2022-12-07 17:22:13 +0100

    new version

--- dist/Modal.js
+++ dist/Modal.js
@@ -5,25 +5,33 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = void 0;
 require("./Modal.css");
+var _react = _interopRequireDefault(require("react"));
 var _reactRouterDom = require("react-router-dom");
 var _reactFontawesome = require("@fortawesome/react-fontawesome");
 var _freeSolidSvgIcons = require("@fortawesome/free-solid-svg-icons");
+var _jsxRuntime = require("react/jsx-runtime");
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 function Modal(_ref) {
   var close = _ref.close;
-  return /*#__PURE__*/React.createElement("div", {
+  return /*#__PURE__*/(0, _jsxRuntime.jsx)("div", {
     className: "blocker",
-    onClick: close
-  }, /*#__PURE__*/React.createElement("div", {
-    id: "confirmation",
-    className: "modal"
-  }, /*#__PURE__*/React.createElement(_reactFontawesome.FontAwesomeIcon, {
-    icon: _freeSolidSvgIcons.faCircleCheck,
-    className: "circle-check"
-  }), /*#__PURE__*/React.createElement("p", null, "Employee Created !"), /*#__PURE__*/React.createElement(_reactRouterDom.Link, {
-    to: "/",
-    className: "close-modal",
-    onClick: close
-  }, "Close")));
+    onClick: close,
+    children: /*#__PURE__*/(0, _jsxRuntime.jsxs)("div", {
+      id: "confirmation",
+      className: "modal",
+      children: [/*#__PURE__*/(0, _jsxRuntime.jsx)(_reactFontawesome.FontAwesomeIcon, {
+        icon: _freeSolidSvgIcons.faCircleCheck,
+        className: "circle-check"
+      }), /*#__PURE__*/(0, _jsxRuntime.jsx)("p", {
+        children: "Employee Created !"
+      }), /*#__PURE__*/(0, _jsxRuntime.jsx)(_reactRouterDom.Link, {
+        to: "/",
+        className: "close-modal",
+        onClick: close,
+        children: "Close"
+      })]
+    })
+  });
 }
 var _default = Modal;
 exports.default = _default;
